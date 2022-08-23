3aa9834685d67ecbffaed2ef669862ea7eb722ba
Marcio Lacerda <marcioalacerda2201@gmail.com>
2022-08-23 09:41:26 -0300

    Refazendo o programa usando while

--- des051_Progressao_Aritmetica.py
+++ des051_Progressao_Aritmetica.py
@@ -1,4 +1,4 @@
-'''Programa que ler o primeiro termo e a razão de uma PA. No final, mostrar os 10 primeiro termos dessa progressão'''
+'''Refazendoo programa usando while.'''
 print('{:=^31}'.format(' PROGRESSÃO ARITMÉTICA '))
 print('{:=^31}'.format(' MOSTRE 10 TERMOS DA PA '))
 p = int(input('Digite o primeiro termo da PA: '))
@@ -6,7 +6,8 @@ r = int(input('Digite a razão da PA: '))
 pa = p
 termo = 1
 print('{:2}º termo = {}'. format(termo, pa))
-for c in range(1,10):
+while termo !=10:
     pa += r
     termo += 1
     print('{:2}º termo = {}'. format(termo, pa))
+print('FIM')
