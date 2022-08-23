c67387740bae9de48902649dc33d5205679e5675
Marcio Lacerda <marcioalacerda2201@gmail.com>
2022-08-23 09:46:03 -0300

    Melhorando o programa, agora perguntando para o usuário se ele quer que mostre mais algum termo. O programa encerra quando o usuário disser que não quer mais.

--- des051_Progressao_Aritmetica.py
+++ des051_Progressao_Aritmetica.py
@@ -1,4 +1,5 @@
-'''Refazendoo programa usando while.'''
+'''Melhorando o programa, agora perguntando para o  usuário se ele quer que mostre mais algum termo. O programa encerra quando ele disser que não quer mostrar os termos.'''
+
 print('{:=^31}'.format(' PROGRESSÃO ARITMÉTICA '))
 print('{:=^31}'.format(' MOSTRE 10 TERMOS DA PA '))
 p = int(input('Digite o primeiro termo da PA: '))
@@ -6,8 +7,14 @@ r = int(input('Digite a razão da PA: '))
 pa = p
 termo = 1
 print('{:2}º termo = {}'. format(termo, pa))
-while termo !=10:
-    pa += r
-    termo += 1
-    print('{:2}º termo = {}'. format(termo, pa))
-print('FIM')
+ctrl = 10
+resp = 'S'
+while resp !='N':
+    while termo != ctrl:
+        pa += r
+        termo += 1
+        print('{:2}º termo = {}'. format(termo, pa))
+    resp = str(input('Voce gostaria que mostrasse mais termos? [S/N] ')).strip().upper()
+    if resp == 'S':
+        ctrl = int(input('Digite o termo seguinte: '))
+print('Fim do programa!')
